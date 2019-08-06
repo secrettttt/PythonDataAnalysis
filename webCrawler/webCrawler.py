009d8c807a8e59f44d64e1e73fd0d3fb93d2886a
secrettttt <243950548@qq.com>
2019-08-07 00:15:23 +0800

    修复了一些bug，已经可以爬取数据了，但是有的时候仍然会有问题

--- webCrawler/webCrawler.py
+++ webCrawler/webCrawler.py
@@ -15,18 +15,77 @@ class anjuke_second_hand_house():
         self.District = input(f"{'Please input the district you want to search:'}")
         self.CollectionName = input(f"{'The collectionName of Mysql(or press Enter to use default name.):'}") or str(self.District)
         self.CONNECTION = pymysql.connect(host='localhost',user='root',password='738291159',db='北京二手房信息',charset='utf8',cursorclass=pymysql.cursors.DictCursor)
-        self.SQL = f"INSERT INTO {self.CollectionName}(所属小区,房屋户型,房屋单价,所在位置,建筑面积,参考首付,建造年代,房屋朝向,参考月供,房屋类型,所在楼层,装修程度,配套电梯,配套供暖)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-
+        self.SQL = f"INSERT INTO {self.CollectionName}(所属小区,房屋户型,房屋单价,所在位置,建筑面积,参考首付,建造年代,房屋朝向,参考月供,房屋类型,所在楼层,装修程度,产权年限,配套电梯,房本年限,产权性质,配套供暖,唯一住房,一手房源)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
     def searchByDistrict(self):
-        if self.District=='全部':
-            for item in self.ALL_District:
-                Originpage = 'https://beijing.anjuke.com/sale/'+self.District+'/'
-                self.mainfunction(Originpage)
-                print(f"All second-hand houses in[{item}]has been saved in database!")
-        else:
-            Originpage = 'https://beijing.anjuke.com/sale/'+self.District+'/'
+        if self.District=='朝阳':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'chaoyang'+'/'
             self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='海淀':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'haidian'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='东城':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'dongcheng'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='西城':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'xicheng'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='丰台':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'fengtai'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='通州':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'tongzhou'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='石景山':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'shijingshan'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='昌平':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'changping'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='大兴':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'daxing'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='顺义':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'shunyi'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='房山':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'fangshan'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='门头沟':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'mentougou'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='密云':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'miyun'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='怀柔':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'huairou'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='平谷':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'pinggu'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='延庆':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'yanqing'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
+        elif self.District=='北京周边':
+            Originpage = 'https://beijing.anjuke.com/sale/'+'beijingzhoubian'+'/'
+            self.mainfunction(Originpage)
+            print(f"All second-hand houses in[{self.District}]has been saved in database!")
 
     def mainfunction(self,Originpage):
         pagenum = self.total_page_number(str(Originpage+'1'))
@@ -64,8 +123,8 @@ class anjuke_second_hand_house():
             print(repr(e))
             self.homepage(homeurl)
         except IndexError:
-                    pass
-        for item in soup.find_all("div",{"class":"house-title"}):
+            pass
+        for item in soup.find_all(class_="house-title"):
             if item.a is not None:
                 if item.a.attrs['href'] is not None:
                     houselink_list.append(item.a.attrs['href'])
@@ -75,34 +134,32 @@ class anjuke_second_hand_house():
         req = requests.get(houseurl)
         page_obj = BeautifulSoup(req.text,"lxml")
         details = []
-        所属小区=房屋户型=房屋单价=所在位置=建筑面积=参考首付=建造年代=房屋朝向=参考月供=房屋类型=所在楼层=装修程度=配套电梯=配套供暖=''
+        所属小区=房屋户型=房屋单价=所在位置=建筑面积=参考首付=建造年代=房屋朝向=参考月供=房屋类型=所在楼层=装修程度=产权年限=配套电梯=房本年限=产权性质=配套供暖=唯一住房=一手房源=''
         try:
-            obj1 = page_obj.find("ul",{"class":"houseInfo-detail-list clearfix"})
-
-            for item in obj1.find_all("div",{"class":"houseInfo-content"}):
+            obj1 = page_obj.find(class_="houseInfo-wrap")
+            for item in obj1.find_all(class_="houseInfo-content"):
                 details.append(item.get_text().strip().strip('\n'))
-            所属小区,房屋户型,房屋单价,所在位置,建筑面积,参考首付,建造年代,房屋朝向,参考月供,房屋类型,所在楼层,装修程度,配套电梯,配套供暖=\
-            details[0],details[1],details[2],details[3],details[4],details[5],details[6],details[7],details[8],details[9],details[10],details[11],details[12],details[13]
-        except Exception as e:
-            print(repr(e))
-            所属小区=房屋户型=房屋单价=所在位置=建筑面积=参考首付=建造年代=房屋朝向=参考月供=房屋类型=所在楼层=装修程度=配套电梯=配套供暖='NULL'
+            所属小区,房屋户型,房屋单价,所在位置,建筑面积,参考首付,建造年代,房屋朝向,参考月供,房屋类型,所在楼层,装修程度,产权年限,配套电梯,房本年限,产权性质,配套供暖,唯一住房,一手房源=\
+            details[0],details[1],details[2],details[3],details[4],details[5],details[6],details[7],details[8],details[9],details[10],details[11],details[12],details[13],details[14],details[15],details[16],details[17],details[18]
+#        except Exception as e:
+#            print(repr(e))
         except IndexError:
-                    pass
+            pass
         finally:
-            housedata = [f'{所属小区}',f'{房屋户型}',f'{房屋单价}',f'{所在位置}',f'{建筑面积}',f'{参考首付}',f'{建造年代}',f'{房屋朝向}',f'{参考月供}',f'{房屋类型}',f'{所在楼层}',f'{装修程度}',f'{配套电梯}',f'{配套供暖}']
+            housedata = [f'{所属小区}',f'{房屋户型}',f'{房屋单价}',f'{所在位置}',f'{建筑面积}',f'{参考首付}',f'{建造年代}',f'{房屋朝向}',f'{参考月供}',f'{房屋类型}',f'{所在楼层}',f'{装修程度}',f'{产权年限}',f'{配套电梯}',f'{房本年限}',f'{产权性质}',f'{配套供暖}',f'{唯一住房}',f'{一手房源}']
             return housedata
         
     def save_mysql(self,house_list,count):
         try:
             with self.CONNECTION.cursor() as cursor:
-                cursor.execute(self.SQL,(house_list[0],house_list[1],house_list[2],house_list[3],house_list[4],house_list[5],house_list[6],house_list[7],house_list[8],house_list[9],house_list[10],house_list[11],house_list[12],house_list[13]))
+                cursor.execute(self.SQL,(house_list[0],house_list[1],house_list[2],house_list[3],house_list[4],house_list[5],house_list[6],house_list[7],house_list[8],house_list[9],house_list[10],house_list[11],house_list[12],house_list[13],house_list[14],house_list[15],house_list[16],house_list[17],house_list[18]))
             self.CONNECTION.commit()
             print(house_list)
             print(f"Page【{count}】 has been saved in Mysql!")
         except Exception as e:
             print(repr(e))
         except IndexError:
-                    pass
+            pass
         
 spider = anjuke_second_hand_house()
 spider.searchByDistrict()
